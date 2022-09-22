185ae1b14ecf4df496546d8ce194bce3616d1d32
Valentina Marino <valenmarinocol@gmail.com>
2022-09-22 14:38:32 +0200

    Readjusted image box's grid dimensions to match the image aspect ratio

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
 import React from "react"
 import FooterComponent from "./components/_Footer/FooterComponent"
-import Header from "./components/_Header/Header"
-import ImgBox from "./components/_ImgBox/index"
+import HeaderComponent from "./components/_Header/HeaderComponent"
+import ImgBox from "./components/_ImgBox/ImgBox"
 import './styles/globals.css'
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle, ImageContainer, Wrapper } from "./styles/global"
 
 
 const App = () => {
 
 const matrix:Array<number[]> = [
   [0, 0], [1, 0], [2, 0], [3, 0],
-  [0, 1], [1, 1], [2, 1], [3, 1],
-  [0, 2], [1, 2], [2, 2], [3, 2],
-  [0, 3], [1, 3], [2, 3], [3, 3],
-  [0, 4], [1, 4], [2, 4], [3, 4],
+  [0, 1], [1, 1], [2, 1], [3, 1], 
+  [0, 2], [1, 2], [2, 2], [3, 2], 
+  [0, 3], [1, 3], [2, 3], [3, 3], 
+  [0, 4], [1, 4], [2, 4], [3, 4], 
   [0, 5], [1, 5], [2, 5], [3, 5]
 ]
 
+console.log(matrix)
+
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-        <Header />
-      <div className="wrapper">
-        {matrix.map(([x, y], i) => (
-          <ImgBox key={i} x={x} y={y} />
-        ))}
-      </div>
+        <HeaderComponent />
         <FooterComponent />
-    </div>
+        <Wrapper>
+          <ImageContainer>
+            {matrix.map(([x, y], i) => (
+              <ImgBox key={i} x={x} y={y} />
+            ))}
+        </ImageContainer>
+        </Wrapper>
+        
+      
+        
+        
+    </>
   )
 }
 
