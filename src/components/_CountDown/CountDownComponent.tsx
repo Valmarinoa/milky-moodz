c4a62ce3842be3741cf61a26bc377155ef3f0913
Valentina Marino <valenmarinocol@gmail.com>
2023-01-07 15:27:31 +0100

    Changed target time

--- src/components/_CountDown/CountDownComponent.tsx
+++ src/components/_CountDown/CountDownComponent.tsx
@@ -10,40 +10,41 @@ const CountDownComponent = () => {
 
 
   const contador = function () {
-    var diaFinal = new Date("Mar 01, 2023").getTime();
+    var diaFinal = new Date("Mar 03, 2023").getTime();
     var hoy = new Date().getTime();
     var falta = diaFinal - hoy;
     setDias(Math.floor(falta / (1000 * 60 * 60 * 24)));
     setHoras(Math.floor((falta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     setMinutos(Math.floor((falta % (1000 * 60 * 60)) / (1000 * 60)));
     setSegundos(Math.floor((falta % (1000 * 60)) / 1000));
-  }
+  };
 
   useEffect(() => {
     setInterval(function () {
-      contador()
-    }, 1000)
-
-
-  }, [])
+      contador();
+    }, 1000);
+  }, []);
 
   return (
     <Counter>
-      <CounterContainer>Dias
+      <CounterContainer>
+        Días
         <H2Counters>{dias}</H2Counters>
       </CounterContainer>
-      <CounterContainer>Hrs
+      <CounterContainer>
+        Hrs
         <H2Counters>{horas}</H2Counters>
       </CounterContainer>
-      <CounterContainer>Min
+      <CounterContainer>
+        Min
         <H2Counters>{minutos}</H2Counters>
       </CounterContainer>
-      <CounterContainer>Sec
+      <CounterContainer>
+        Sec
         <H2Counters>{segundos}</H2Counters>
       </CounterContainer>
     </Counter>
-
-  )
+  );
 }
 
 export default CountDownComponent
